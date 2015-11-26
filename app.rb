471e8da867431f6f09685b6b68a4b6de4f6eb863
eugen <tassadars@gmail.com>
2015-11-26 23:00:34 +0200

    update, refactoring

--- app.rb
+++ app.rb
@@ -1,28 +1,4 @@
-class Logger
-  def initialize
-        @f = File.open 'log.txt', 'a'
-  end
-
-  @@x = nil 
-  
-  def self.instance
-    if @@x == nil
-      @@x = Logger.new
-    end
-
-    return @@x
-  end
-
-  # class method
-  def self.say_something
-    puts "haha"
-  end
-
-  # instance method
-  def log_something what
-    @f.puts what
-  end
-end
+require './logger'
  
 Logger.say_something
 Logger.instance.log_something "from singleton"
